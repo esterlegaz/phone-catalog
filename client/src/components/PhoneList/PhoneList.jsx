4d932823fd73463ee4e1e15ea81388e3a3240421
“Ester <esterlegaz@gmail.com>
2021-01-17 13:31:02 +0100

    feat(general): include search input

--- client/src/components/PhoneList/PhoneList.jsx
+++ client/src/components/PhoneList/PhoneList.jsx
@@ -5,12 +5,14 @@ import PhoneItem from './../PhoneItem/PhoneItem';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 import { startLoading, stopLoading } from './../../store/global/globalActions';
+import TextField from "@material-ui/core/TextField";
 
 class PhoneList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            phones: []
+            phones: [],
+            searchTerm: ''
         };
     }
 
@@ -34,9 +36,49 @@ class PhoneList extends React.Component {
             });
     };
 
+    handleChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+
+    filterPhones() {
+        let filteredPhones = [];
+        if (this.state.searchTerm) {
+            filteredPhones = this.state.phones.filter(phone => {
+                return (
+                    phone.name.toUpperCase().includes(this.state.searchTerm.toUpperCase())
+                );
+            }).length !== 0
+        }
+
+        filteredPhones.map((phone, index) => {
+            return <PhoneItem
+                key={index}
+                id={"phone-" + phone.id.toString()}
+                information={phone}
+            />
+        })
+
+
+    }
+
+    filterPhones = () => {
+        const { phones, searchTerm } = this.state;
+        return phones.filter(book => {
+            const title = book.name.toUpperCase().includes(searchTerm.toUpperCase());
+            return title;
+        })
+    }
+
     render() {
         return (
+            !this.props.isLoading &&
             <>
+                <div className="list__search">
+                    <TextField onKeyUp={this.handleChange} id="search" label="Search your phone" type="search" />
+                </div>
+
                 <div className="form__hint">
                     <p>Do you want to add a new phone?</p>
                     <Link to="/form">
@@ -44,32 +86,43 @@ class PhoneList extends React.Component {
                     </Link>
                 </div>
 
-                {this.state.phones && this.state.phones.length > 0 && (
-                    <>
-                        <h2 className="list__title">Find your phone</h2>
-                        <ul className="list__container">
-                            {this.state.phones.map((phone, index) => {
-                                return (
-                                    <PhoneItem
-                                        key={index}
-                                        id={"phone-" + phone.id.toString()}
-                                        information={phone}
-                                    />
-                                );
-                            })}
-                        </ul>
-                    </>
-                )}
+                {this.filterPhones() && this.filterPhones().length > 0
+                    ? (
+                        <>
+                            <ul className="list__container">
+                                {this.filterPhones().map((phone, index) => {
+                                    return (
+                                        <PhoneItem
+                                            key={index}
+                                            id={"phone-" + phone.id.toString()}
+                                            information={phone}
+                                        />
+                                    );
+                                })}
+
+                            </ul>
+                        </>
+                    )
+                    :
+                    <p className="list__empty">
+                        No results found. Please, try again.
+                    </p>
+
+                }
+
             </>
         )
     }
 }
 
 const mapStateToProps = (state) => {
+    const isLoading = state.global.isLoading;
 
     return {
+        isLoading
     };
 };
+
 
 const mapDispatchToProps = {
     startLoading,
